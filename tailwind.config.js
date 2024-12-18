6aff3ddf9bea46fb318528c9dd9e109b5b4111bf
Mouli Chandramouli <mchandramouli@gmail.com>
2024-12-17 19:22:48 -0700

    update font - restore old

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,28 +7,14 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'Arial', 'sans-serif'],
         serif: ['Playfair Display', 'serif'],
-      },
-      fontSize: {
-        '2xs': '0.7rem', // Smaller mobile font size
-        xs: '0.75rem',
-        sm: '0.875rem',
-        base: '1rem',
-        lg: '1.125rem',
-        xl: '1.25rem',
-        '2xl': '1.5rem',
-        '3xl': '1.875rem',
-        '4xl': '2.25rem',
-        '5xl': '3rem',
-        '6xl': '3.75rem',
-        '7xl': '4.5rem',
+        sans: ['Inter', 'sans-serif'],
       },
       transitionProperty: {
         'height': 'height',
         'spacing': 'margin, padding',
-      },
+      }
     },
   },
   plugins: [],
-};
+}
