22e051fba2fa3d26629c8ac76f833471dd1deb87
Mouli Chandramouli <mchandramouli@gmail.com>
2024-12-28 16:13:27 -0700

    sitemap update

--- api/sitemap.xml.js
+++ api/sitemap.xml.js
@@ -4,25 +4,31 @@ export default function handler(req, res) {
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://calgaryacademicexcellence.vercel.app</loc>
-    <lastmod>2024-03-13</lastmod>
+    <lastmod>2024-12-28</lastmod>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
     <loc>https://calgaryacademicexcellence.vercel.app/about</loc>
-    <lastmod>2024-03-13</lastmod>
+    <lastmod>2024-12-28</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
     <loc>https://calgaryacademicexcellence.vercel.app/resources</loc>
-    <lastmod>2024-03-13</lastmod>
+    <lastmod>2024-12-28</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.9</priority>
   </url>
   <url>
     <loc>https://calgaryacademicexcellence.vercel.app/blog</loc>
-    <lastmod>2024-03-13</lastmod>
+    <lastmod>2024-12-28</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://calgaryacademicexcellence.vercel.app/college-predictor</loc>
+    <lastmod>2024-12-28</lastmod>
     <changefreq>daily</changefreq>
     <priority>0.9</priority>
   </url>
